e8f51459a05af5d31e626bff8460edef8ff0ac93
daniellygj <daniellygj@gmail.com>
2020-11-09 22:37:37 -0300

    log do usuario

--- src/main/java/br/gov/sp/fatec/user/controller/UserController.java
+++ src/main/java/br/gov/sp/fatec/user/controller/UserController.java
@@ -1,26 +1,31 @@
 package br.gov.sp.fatec.user.controller;
 
-import br.gov.sp.fatec.representative.domain.Representative;
+import br.gov.sp.fatec.Antena;
 import br.gov.sp.fatec.user.domain.User;
 import br.gov.sp.fatec.user.service.UserService;
-import br.gov.sp.fatec.utils.view.View;
-import com.fasterxml.jackson.annotation.JsonView;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("dev/user")
 public class UserController {
 
+    private static final Logger logger = LogManager.getLogger(Antena.class);
+
     @Autowired
     private UserService service;
 
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public User getLoggedInfo() {
-        return service.getLoggedInfo();
+        User user = service.getLoggedInfo();
+        logger.info(user);
+        return user;
     }
 
     @GetMapping(value = "/activate/{b64}")
